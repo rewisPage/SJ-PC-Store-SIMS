6424a83af2ba251dd268420079dc19f5cb801a34
rewisPage <alberto.395919@balagtas.sti.edu.ph>
2026-06-07 23:40:14 +0800

    Created this classes for Activity Log

---
+++ SJ PC Store SIMS/Models/ActivityLogModel.cs
@@ -0,0 +1,12 @@
+﻿namespace SJ_PC_Store_SIMS.Models
+{
+    public class ActivityLogModel
+    {
+        public int LogID { get; set; }
+        public string UserID { get; set; }
+        public string FullName { get; set; }
+        public string ModuleCategory { get; set; }
+        public string ActionDescription { get; set; }
+        public DateTime LogDate { get; set; }
+    }
+}
